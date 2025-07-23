26b5c128f4032f069378e211311531c5ab4359b6
richlovestocode <tanmay@hackthenorth.com>
2025-07-23 14:44:49 -0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>React Sandbox</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
